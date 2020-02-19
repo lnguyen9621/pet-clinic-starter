158ea92eb6f00ebed091a9240eaa150670b80490
Long Nguyen <longnguyen@Longs-MacBook-Pro.local>
2020-02-19 12:31:28 -0700

    inital codes and maven dependencies for pet clinic project starter built by https://start.spring.io

---
+++ src/test/java/home/javacoder/springbootpetclinic/SpringbootPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package home.javacoder.springbootpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
